de5c77c4f1dfb4ee3eb577224eb0649b31da698c
not4drugs <not4drugs@protonmail.com>
2018-01-26 23:28:15 +0700

    Completed tests for 'messages' module

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -3,7 +3,9 @@ from Cryptodome.PublicKey import RSA
 from messy.messages import (MAC_TAG_LENGTH,
                             NONCE_LENGTH,
                             decrypt,
-                            encrypt)
+                            encrypt,
+                            encode,
+                            decode)
 
 
 def test_encrypt(public_key: RSA.RsaKey,
@@ -24,6 +26,21 @@ def test_decrypt(private_key: RSA.RsaKey,
                         public_key=public_key)
     result = decrypt(encrypted,
                      private_key=private_key)
+
+    assert isinstance(result, bytes)
+    assert result == message
+
+
+def test_encode(message: bytes) -> None:
+    result = encode(message)
+
+    assert isinstance(result, bytes)
+    assert len(result) >= len(message)
+
+
+def test_decode(message: bytes) -> None:
+    encoded = encode(message)
+    result = decode(encoded)
 
     assert isinstance(result, bytes)
     assert result == message
