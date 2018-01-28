cfb092dc6a60e7b0eecb4c3f16cbed75fb152f19
John Doe <not4drugs@protonmail.com>
2018-01-29 05:09:01 +0700

    Added application & application-based fixtures (server, client)

--- tests/fixtures/app.py
+++ tests/fixtures/app.py
@@ -1,7 +1,12 @@
+from asyncio import AbstractEventLoop
 from logging import Logger
 
 import pytest
+from aiohttp.test_utils import (TestServer,
+                                TestClient)
+from aiohttp.web import Application
 
+from messy.app import create
 from tests import strategies
 from tests.utils import example
 
@@ -14,3 +19,27 @@ def logger() -> Logger:
 @pytest.fixture(scope='function')
 def instance_name() -> str:
     return example(strategies.instances_names)
+
+
+@pytest.fixture(scope='function')
+def app(logger: Logger,
+        instance_name: str,
+        event_loop: AbstractEventLoop) -> Application:
+    return create(logger=logger,
+                  instance_name=instance_name,
+                  loop=event_loop)
+
+
+@pytest.fixture(scope='function')
+def server(app: Application,
+           event_loop: AbstractEventLoop) -> TestServer:
+    return TestServer(app,
+                      loop=event_loop)
+
+
+@pytest.fixture(scope='function')
+def client(server: TestServer,
+           event_loop: AbstractEventLoop) -> TestClient:
+    with TestClient(server,
+                    loop=event_loop) as result:
+        yield result
